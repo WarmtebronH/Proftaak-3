3709884d31193dde593c8677f6cf7e11a9193cf0
WarmtebronH <64137442+WarmtebronH@users.noreply.github.com>
2020-04-24 12:24:59 +0200

    changed small things, added new webpage

---
+++ besteld.js
@@ -0,0 +1,5 @@
+var ophaaltijd1 = 15.35;
+var ophaaltijd2 = 15.55;
+
+document.getElementById("ophaaltijd-start").innerHTML = ophaaltijd1;
+document.getElementById("ophaaltijd-eind").innerHTML = ophaaltijd2;
